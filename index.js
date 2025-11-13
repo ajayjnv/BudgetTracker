c5e8bf51c68d9f13dabf36858a0dbf127ba34e42
ajayjnv <ajaykumarkontala@gmail.com>
2025-11-13 22:27:47 +0530

    updated index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ let isConnected = false;
 
 async function connectWithRetry() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI);
         isConnected = true;
         console.log("MongoDB is connected");
     }
